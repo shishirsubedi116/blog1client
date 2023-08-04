f56cebdbb3fc050aa83c91358f78fc07d0e78a8e
Shishir <shishirsubedi116@gmail.com>
2023-08-04 21:00:35 +0545

    Navbar searchicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,10 +16,10 @@ const Navbar = () => {
             <div className="relative mt-2 md:mt-0 md:w-1/3 lg:w-1/4">
                 <input type="text" placeholder="Search"
                     className="py-2 pl-8 pr-12 rounded-full focus:outline-none md:max-w-sm lg:max-w-md border border-gray-300" />
-                <button
+                {/* <button
                     className="bg-gray-600 text-white rounded-full px-4 py-2 absolute ">
                         search
-                </button>
+                </button> */}
             </div>
 
             <div className="flex items-center mt-2 md:mt-0">
